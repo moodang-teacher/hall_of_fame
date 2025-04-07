a2b8b0b5aefbe2dc11e26c862cf23587a9f33f26
moodang teacher <yssmi@naver.com>
2025-04-07 16:49:49 +0900

    기수별 페이지 분리

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
+// GSAP 플러그인 등록
+gsap.registerPlugin(TextPlugin);
 document.addEventListener('DOMContentLoaded', () => {
-    // GSAP 플러그인 등록
-    gsap.registerPlugin(TextPlugin);
-
     // DOM 요소 선택
     const cursor = document.querySelector('#cursor');
     const nameSliderArea = document.querySelector('.name-slider');
-    const nameSliderWrapper = document.querySelector('.name-slider .swiper-wrapper');
-    const picSliderWrapper = document.querySelector('.pic-slider .swiper-wrapper');
+    // const nameSliderWrapper = document.querySelector('.name-slider .swiper-wrapper');
+    // const picSliderWrapper = document.querySelector('.pic-slider .swiper-wrapper');
 
     const nameSliderLi = gsap.utils.toArray('.name-slider .swiper-slide');
     nameSliderLi.forEach((li, index) => {
@@ -27,27 +26,12 @@ document.addEventListener('DOMContentLoaded', () => {
     // 텍스트 애니메이션 함수
     const animateText = (text) => {
         gsap.to('.title', {
-            duration: 0.2,
+            duration: 0.8,
             text: {
                 value: text,
                 delimiter: '',
             },
             ease: 'none',
-        });
-    };
-
-    // 슬라이드 요소들을 랜덤하게 섞는 함수
-    const shuffleSlides = () => {
-        const nameSlides = Array.from(nameSliderWrapper.children);
-        const picSlides = Array.from(picSliderWrapper.children);
-
-        // 같은 랜덤 순서를 두 슬라이더에 적용
-        const randomOrder = [...Array(nameSlides.length).keys()].sort(() => Math.random() - 0.5);
-
-        // 슬라이드 재정렬
-        randomOrder.forEach((index) => {
-            nameSliderWrapper.appendChild(nameSlides[index]);
-            picSliderWrapper.appendChild(picSlides[index]);
         });
     };
 
@@ -62,9 +46,9 @@ document.addEventListener('DOMContentLoaded', () => {
         gsap.to(cursor, {
             duration: 0.8,
             backgroundColor: 'rgba(71, 101, 11, 0.6)',
-            width: 200,
-            height: 100,
-            textContent: 'WHEEL || CLICK',
+            width: 120,
+            height: 120,
+            textContent: 'CLICK',
             ease: 'elastic.out(1,0.3)',
         });
     });
@@ -97,14 +81,10 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // 슬라이드 섞기 실행
-    shuffleSlides();
-
     // Swiper 초기화
     const nameSlider = new Swiper('.name-slider', {
         loop: true,
         slidesPerView: 'auto',
-        mousewheel: true,
         spaceBetween: 2,
     });
 
